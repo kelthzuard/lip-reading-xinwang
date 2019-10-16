05078eb45727a80fe0723a43c032980258d6b1c3
kelthzuard <a522516259@163.com>
2019-10-16 21:51:40 +0800

    使用resize规范处理完图片尺寸，保留色域不使用灰度图像，为3d卷积做准备

--- img-cutting.py
+++ img-cutting.py
@@ -14,9 +14,7 @@ for root,dirs,files in os.walk(args["input"]):
     for file in files:
         file_path = os.path.join(root,file)
 
-        # 使用opencv阅读图像并且转化为灰度图像
         imgReaded = cv2.imread(file_path)
-        img_gray = cv2.cvtColor(imgReaded, cv2.COLOR_RGB2GRAY)
 
         height = len(imgReaded)
         width = len(imgReaded[0])
@@ -25,5 +23,7 @@ for root,dirs,files in os.walk(args["input"]):
         cutWidth = int(height*0.2)
 
         #去掉图像两边上下20%的部分，仅仅保留中间部分
-        cropped = img_gray[cutHeight:(height-cutHeight), cutWidth:(width-cutWidth)]
-        cv2.imwrite(file_path, cropped)
+        cropped = imgReaded[cutHeight:(height-cutHeight), cutWidth:(width-cutWidth)]
+        dim = (200, 200)
+        resized = cv2.resize(cropped, dim, interpolation=cv2.INTER_AREA)
+        cv2.imwrite(file_path, resized)
