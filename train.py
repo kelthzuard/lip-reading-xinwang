3993815b9233955ac6f578ff76d4e210a356f511
kelthzuard <a522516259@163.com>
2019-10-24 16:26:04 +0800

    更新3D卷积网络模型-未完成

--- train.py
+++ train.py
@@ -4,6 +4,10 @@ import argparse
 import numpy as np
 from keras.preprocessing.text import Tokenizer
 
+from keras.models import Sequential, load_model
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution3D, MaxPooling3D
+
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--input", required=True,
                 help="path to input file categories")
@@ -50,10 +54,10 @@ def read_and_initial_data(main_path, label_list):
     # 初始化一维标签数组
     label = []
     categories = os.listdir(main_path)
-    traning_set = np.zeros(shape=(1, 15, 200, 200, 3))
+    training_set = np.zeros(shape=(1, img_frames, img_rows, img_cols, 3))
     for index, package in enumerate(categories):
         package_path = os.path.join(main_path, package)
-        one_package_vector = np.zeros(shape=(1, 200, 200, 3))
+        one_package_vector = np.zeros(shape=(1, img_rows, img_cols, 3))
         for root, dirs, files in os.walk(package_path):
             for innerIndex, file in enumerate(files):
                 file_path = os.path.join(package_path, file)
@@ -67,20 +71,87 @@ def read_and_initial_data(main_path, label_list):
                     one_package_vector = np.concatenate((one_package_vector, img_reshaped), axis=0)
         # 把sample对应的label添加进label数组中
         label.append(label_list[package])
-        while one_package_vector.shape[0] < 15:
-            padding_vector = np.zeros(shape=(1, 200, 200, 3))
+        while one_package_vector.shape[0] < img_frames:
+            padding_vector = np.zeros(shape=(1, img_rows, img_cols, 3))
             one_package_vector = np.concatenate((one_package_vector, padding_vector), axis=0)
         one_package_vector_reshaped = one_package_vector.reshape(1, one_package_vector.shape[0],
                                                                  one_package_vector.shape[1],
                                                                  one_package_vector.shape[2],
                                                                  one_package_vector.shape[3])
         if index == 0:
-            traning_set = one_package_vector_reshaped
+            training_set = one_package_vector_reshaped
         else:
-            traning_set = np.concatenate((traning_set, one_package_vector_reshaped), axis=0)
-    return traning_set, label
+            training_set = np.concatenate((training_set, one_package_vector_reshaped), axis=0)
+    training_set = pre_dealing_data(training_set)
+    return training_set, label
+
+
+# 预处理数据
+def pre_dealing_data(train_set):
+    train_set = train_set.astype('float32')
+    train_set -= np.mean(train_set)
+    train_set /= np.max(train_set)
+    return train_set
 
 
 label_list = read_and_initial_label((args["label"]))
 training_data, label = read_and_initial_data(args["input"], label_list)
 label = encode_label(label)
+
+# *****************
+# 定义3D卷积网络模型参数
+# *****************
+img_rows = 200
+img_cols = 200
+img_frames = 15
+patch_size = 15  # 每批次训练数量
+filters_3D = [32, 50]  # 卷积核数量
+conv_3D = [5, 5]  # 卷积核尺寸
+pool_3D = [3, 3]  # 池化层尺寸
+
+
+# 加载模型
+model_exists = os.path.exists('model.h5')
+if (model_exists):
+    model = load_model('model.h5')
+    print("**************************************************")
+    print("model.h5 model loaded")
+
+else:
+    model = Sequential()
+
+    model.add(Convolution3D(
+        filters_3D[0],
+        kernel_dim1=conv_3D[0],  # depth
+        kernel_dim2=conv_3D[0],  # rows
+        kernel_dim3=conv_3D[0],  # cols
+        input_shape=(3, img_rows, img_cols, patch_size),
+        activation='relu'
+    ))
+
+    model.add(MaxPooling3D(pool_size=(pool_3D[0], pool_3D[0], pool_3D[0])))
+
+    model.add(Convolution3D(
+        filters_3D[1],
+        kernel_dim1=conv_3D[1],  # depth
+        kernel_dim2=conv_3D[1],  # rows
+        kernel_dim3=conv_3D[1],  # cols
+        activation='relu'
+    ))
+
+    model.add(MaxPooling3D(pool_size=(pool_3D[1], pool_3D[1], pool_3D[1])))
+
+    model.add(Dropout(0.5))
+
+    model.add(Flatten())
+
+    model.add(Dense(128, init='normal', activation='relu'))
+
+    model.add(Dropout(0.5))
+
+    model.add(Dense(6, init='normal'))
+
+    model.add(Activation('softmax'))
+
+    model.compile(loss='categorical_crossentropy', optimizer='RMSprop', metrics=['mse', 'accuracy'])
+
