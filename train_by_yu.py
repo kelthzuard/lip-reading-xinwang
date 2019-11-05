4d88bbc6caf0e769c722e9d1223083bac594fef0
yufangjian <sadad@asdsa.com>
2019-11-05 18:48:11 +0800

    v1.2

--- train_by_yu.py
+++ train_by_yu.py
@@ -12,9 +12,18 @@ from keras.layers import LSTM
 
 print("ok,let's go!")
 
-filters_3D_1 = 0
-kernel_1 = 0
-kernel_2 = 0
+filters_3D_1 = 64
+kernel_size1 = (1,3,3)
+stride1 = (1,2,2)
+
+max_pool_size1 = (1,3,3)
+max_pool_stride1 = (1,2,2)
+max_pool_pad1 = (0,1,1)
+
+#输入图片的情况：200*200*19
+img_rows = 200
+img_cols = 200
+img_frames = 20
 
 #如果模型存在，加载已有模型
 if(os.path.exists('model.h5')):
@@ -25,16 +34,11 @@ if(os.path.exists('model.h5')):
 else:
     model = Sequential()
     #第一层卷积层
-    model.add(Conv3D(
-        filters_3D_1,
-        (kernel_1,kernel_2)
-    ))
+    model.add(Conv3D(filters_3D_1,kernel_size1,stride1,input_shape=(img_frames, img_rows, img_cols, 3),activation="relu"))
     #第一层最大池化层
-    model.add(MaxPooling3D())
+    model.add(MaxPooling3D(max_pool_size1, strides=max_pool_stride1, padding=max_pool_pad1))
     #第二层卷积层
-    model.add(Conv3D(
-
-    ))
+    model.add(Conv3D())
     #第二层最大池化层
     model.add(MaxPooling3D())
     #第三层卷积层
